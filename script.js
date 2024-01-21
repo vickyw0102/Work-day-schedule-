c9d63d77e7393451f52fde9045354248b8f8e48e
vicky wong <vicky.boci@gmail.com>
2024-01-21 22:11:07 +0800

    added schedulers without saved and border stuff

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 var currentDay = $("#currentDay");
 var today =dayjs();
 currentDay.text(today.format('dddd MMMM DD'));
-function timeBlock(){
-    var timeBlock = $ ("#timeBlocksContainer");
-    var timeBlockDiv = ("<div>")
+// function timeBlock(){
+//     var timeBlock = $ ("#timeBlocksContainer");
+//     var timeBlockDiv = ("<div>")
+//     var currentHours = dayjs ().hour();
+//     console.log(currentHours);
+//     timeBlock.addClass('.time-block');
+// }
+$(".time-div").each(function(){
+    var timeBlock = $(this);
     var currentHours = dayjs ().hour();
     console.log(currentHours);
     timeBlock.addClass('.time-block');
+});
 
-
-}
