c7226cd6ed76b285401208ade68936851348c795
vicky wong <vicky.boci@gmail.com>
2024-01-29 17:41:47 +0800

    updated Css and all storage

--- script.js
+++ script.js
@@ -16,32 +16,33 @@ $(".time-div").each(function(){
 
     if(currentHours==timeDiv){
         $(this).addClass("present");
-        $(this).children(".item").addClass("white-text");
+        $(this).children(".item").addClass("grey-text");
         } else if (currentHours <timeDiv) {
             $(this).removeClass("present");
             $(this).removeClass("past");
             $(this).addClass("future");
-            $(this).children(".item").addClass("grey-text");
+            $(this).children(".item").addClass("white-text");
             
         } else if (currentHours> timeDiv){
             $(this).removeClass("present");
             $(this).removeClass("future");
             $(this).addClass("past");
-            $(this).children(".item").addClass("grey-text");
+            $(this).children(".item").addClass("white-text");
         }
 });
 
-$(".btn").click(function (event) {
+$(".saveBtn").click(function (event) {
     event.preventDefault();
-    var value = $(this).siblings(".time-div").val();
-    var time = $(this).parent().attr("id").split("-")[1];
-    localStorage.setItem(time, value);
+    var value = $(this).siblings(".item").val();
+    var time = $(this).parent().attr("id");
+    console.log(value, time);
+    localStorage.setItem(value, time);
     });
 
     //retrieves items from local storage and sets them in proper places
-    $("#hour-09 .time-div").val(localStorage.getItem("09"));
-    $("#hour-10 .time-div").val(localStorage.getItem("10"));
-    $("#hour-11 .time-div").val(localStorage.getItem("11"));
+    $("#hour-09 .item").val(localStorage.getItem("09"));
+    $("#hour-10 .item").val(localStorage.getItem("10"));
+    $("#hour-11 .item").val(localStorage.getItem("11"));
     $("#hour-12 .time-div").val(localStorage.getItem("12"));
     $("#hour-13 .time-div").val(localStorage.getItem("13"));
     $("#hour-14 .time-div").val(localStorage.getItem("14"));
